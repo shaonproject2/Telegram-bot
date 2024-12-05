71bc4c74695c47dd01ddfbaa89c7ca7686f01e3d
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-12-05 17:56:46 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
   ownerNumber: "+8801814587247",//Your Number
   admin: ["5891084825"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "7529156928:AAHFB5GOA9cOU89VN1d-oi1KvTw6M7YhiMI",//Your Bot Tokens
+  telegramBotToken: "7731877803:AAFKp3NksUCMoFC0k-FLAyp6vWBcxNaVxBo",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/shaonproject",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
