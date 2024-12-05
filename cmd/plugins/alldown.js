df1724fa073d4f02a9ffe76a3f49e881d4e1671c
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-12-05 14:14:11 +0600

    Update alldown.js

--- cmd/plugins/alldown.js
+++ cmd/plugins/alldown.js
@@ -41,7 +41,7 @@ module.exports = {
       const replyMarkup = {
         reply_markup: {
           inline_keyboard: [
-            [{ text: '🔗 Bot Owner', url: 'https://t.me/MOHAMMADNAYAN' }],
+            [{ text: '🔗 Bot Owner', url: 'https://t.me/shaonproject' }],
           ],
         },
       };
